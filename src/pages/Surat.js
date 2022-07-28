211ed1c6850a4b9bf711cb8aa0d9e3880c82efc7
SatrioBayu <sbayu875@gmail.com>
2022-07-28 19:29:24 +0700

    feat: add logic in surat form

--- src/pages/Surat.js
+++ src/pages/Surat.js
@@ -2,8 +2,24 @@ import Footer from "../components/Footer";
 import Navbar from "../components/Navbar";
 import styles from "../assets/css/Surat.module.css";
 import SideMap from "../components/SideMap";
+import { useState } from "react";
 
 const Surat = () => {
+  const [nama, setNama] = useState("");
+  const [nik, setNik] = useState("");
+  const [email, setEmail] = useState("");
+  const [jenisSurat, setJenisSurat] = useState("");
+  const [keterangan, setKeterangan] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(nama);
+    console.log(nik);
+    console.log(email);
+    console.log(jenisSurat);
+    console.log(keterangan);
+  };
+
   return (
     <div>
       <Navbar />
@@ -13,34 +29,49 @@ const Surat = () => {
         <div className="row g-4 my-4">
           <div className="col-lg-8">
             <div className={`${styles["surat-container"]}`}>
-              <form className="p-4">
+              <form onSubmit={handleSubmit} className="p-4">
+                {/* Nama */}
                 <h5 className="mb-3">Nama</h5>
-                <input required type="text" placeholder="Jawaban Anda" className={`mb-4 ${styles["input-jawaban"]}`} />
+                <input required onChange={(e) => setNama(e.target.value)} type="text" placeholder="Jawaban Anda" className={`mb-4 ${styles["input-jawaban"]}`} />
+
+                {/* NIK */}
                 <h5 className="mb-3">NIK</h5>
-                <input required type="text" placeholder="Jawaban Anda" className={`mb-4 ${styles["input-jawaban"]}`} />
+                <input required onChange={(e) => setNik(e.target.value)} type="text" placeholder="Jawaban Anda" className={`mb-4 ${styles["input-jawaban"]}`} />
+
+                {/* Email */}
                 <h5 className="mb-3">Email</h5>
-                <input required type="text" placeholder="Jawaban Anda" className={`mb-4 ${styles["input-jawaban"]}`} />
+                <input required onChange={(e) => setEmail(e.target.value)} type="text" placeholder="Jawaban Anda" className={`mb-4 ${styles["input-jawaban"]}`} />
+
+                {/* Jenis Surat */}
                 <h5 className="mb-3">Pilih Jenis Surat</h5>
                 <div className="form-check">
-                  <input className="form-check-input" type="radio" name="jenis-surat" />
-                  <label className={`form-check-label ${styles["input-radio"]}`} for="flexRadioDefault1">
+                  <input required onChange={(e) => setJenisSurat(e.target.value)} value="Surat Keterangan Usaha" className="form-check-input" type="radio" name="jenis-surat" />
+                  <label className={`form-check-label ${styles["input-radio"]}`} htmlFor="flexRadioDefault1">
                     Surat Keterangan Usaha
                   </label>
                 </div>
                 <div className="form-check">
-                  <input className="form-check-input" type="radio" name="jenis-surat" />
-                  <label className={`form-check-label ${styles["input-radio"]}`} for="flexRadioDefault2">
+                  <input required onChange={(e) => setJenisSurat(e.target.value)} className="form-check-input" type="radio" name="jenis-surat" value="Surat Keterangan Menikah" />
+                  <label className={`form-check-label ${styles["input-radio"]}`} htmlFor="flexRadioDefault2">
                     Surat Keterangan Menikah
                   </label>
                 </div>
                 <div className="form-check mb-4">
-                  <input className="form-check-input" type="radio" name="jenis-surat" />
-                  <label className={`form-check-label ${styles["input-radio"]}`} for="flexRadioDefault2">
+                  <input required onChange={(e) => setJenisSurat(e.target.value)} className="form-check-input" type="radio" name="jenis-surat" value="Surat Keterangan Tidak Mampu" />
+                  <label className={`form-check-label ${styles["input-radio"]}`} htmlFor="flexRadioDefault2">
                     Surat Keterangan Tidak Mampu
                   </label>
                 </div>
+
+                {/* Keterangan */}
                 <h5>Keterangan</h5>
-                <textarea class="form-control my-4 p-3" rows="4" placeholder="Ingin membuat surat keterangan untuk menikah pada bulan september 2022" id="floatingTextarea"></textarea>
+                <textarea
+                  onChange={(e) => setKeterangan(e.target.value)}
+                  class="form-control my-4 p-3"
+                  rows="4"
+                  placeholder="Ingin membuat surat keterangan untuk menikah pada bulan september 2022"
+                  id="floatingTextarea"
+                ></textarea>
                 <button className="btn btn-primary px-3">Kirim</button>
               </form>
             </div>

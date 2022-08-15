3485a10ae95de27379f3d511c5c0997512d8d341
SatrioBayu <sbayu875@gmail.com>
2022-08-15 21:28:53 +0700

    feat: fix pol

--- src/components/Jumbotron.js
+++ src/components/Jumbotron.js
@@ -7,7 +7,7 @@ const Jumbotron = () => {
         <div className="row gx-4 gx-lg-5 h-100 align-items-center justify-content-center text-center">
           <div className="col-lg-8 align-self-end">
             <h1 className="text-white font-weight-bold">DESA LOJAJAR</h1>
-            <h1 className="text-white font-weight-bold">KECAMATAN TENGGARENG</h1>
+            <h1 className="text-white font-weight-bold">KECAMATAN TENGGARANG</h1>
             <h1 className="text-white font-weight-bold">KABUPATEN BONDOWOSO</h1>
             <hr className={styles["divider-custom"]} />
           </div>
